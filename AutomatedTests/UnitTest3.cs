ead9c7d53cea48fba60a0e97e11ac316326a1f58
ichijikuibo <61333434+ichijikuibo@users.noreply.github.com>
2020-10-20 20:52:02 +0100

    Iteration 3 (#3)
    * Iteration 3
    
    * Update dotnet-core.yml
    
    * Update dotnet-core.yml
    
    * Update dotnet-core.yml
    
    * Iteration 3
    
    * Iteration 3
    
    * Update dotnet-core.yml
    
    * Iteration 3

--- AutomatedTests/UnitTest3.cs
+++ AutomatedTests/UnitTest3.cs
@@ -10,12 +10,15 @@ namespace AutomatedTests
     [TestClass]
     public class Requirement3Tests
     {
+
+        Data.TestLocationTriggerData testData;
         [TestMethod]
         public void DistanceTo()
         {
-            Data.TestLocationTriggerData testData = new Data.TestLocationTriggerData();
+            
             string[] tests = Helpers.OpenFile("TestData/2PointTestData.txt");
-            foreach(string s in tests)
+            foreach (string s in tests)
+
             {
                 string[] test = s.Split('\t');
                 string[] input = test[0].Split(',');
@@ -23,8 +26,115 @@ namespace AutomatedTests
                 MapCoordinate MC1 = new MapCoordinate(double.Parse(input[0]), double.Parse(input[1]));
                 MapCoordinate MC2 = new MapCoordinate(double.Parse(input[2]), double.Parse(input[3]));
                 double distance = Helpers.RoundToSignificantDigits(MC1.DistanceTo(MC2), 4);
-                Assert.AreEqual(distance, double.Parse(result[0]), String.Format("Expected for '{0}': {1}; Actual: {2}", Helpers.StringFromArray(input), result[0],distance));
+
+                Assert.AreEqual(distance, double.Parse(result[0]), String.Format("Expected for '{0}': {1}; Actual: {2}", Helpers.StringFromArray(input), result[0], distance));
             }
+        }
+        [TestMethod]
+        public void ClosestPointTo()
+        {
+            string[] tests = Helpers.OpenFile("TestData/ClosestPointTestData.txt");
+            foreach (string s in tests)
+            {
+                string[] test = s.Split('\t');
+                string[] input = test[0].Split(',');
+                string[] result = test[1].Split(',');
+                MapCoordinate MC1 = new MapCoordinate(double.Parse(input[1]), double.Parse(input[2]));
+                MapCoordinate MCResult = new MapCoordinate(double.Parse(result[0]), double.Parse(result[1]));               
+                BasicLocationTrigger TestTrigger = (BasicLocationTrigger)testData.GetLocation(input[0]);
+                MapCoordinate ActualResult = TestTrigger.ClosestPointTo(MC1);
+                Assert.AreEqual(Math.Round(MCResult.Longitude,5), Math.Round(ActualResult.Longitude, 5));
+                Assert.AreEqual( Math.Round(MCResult.Latitude,5), Math.Round(ActualResult.Latitude, 5));
+            }
+        }
+        [TestMethod]
+        public void NearbyLocations()
+        {
+            string[] tests = Helpers.OpenFile("TestData/NearbyLocationsTestData.txt");
+            LocationTriggerCollection<BasicLocationTrigger> testCollection = new LocationTriggerCollection<BasicLocationTrigger>();
+            testCollection.AddRange(testData.TestData);
+            foreach (string s in tests)
+            {
+                string[] test = s.Split('\t');
+                string[] input = test[0].Split(',');
+                string[] result = test[1].Split(',');
+                var ActualResult = testCollection.LocationsNear(new MapCoordinate(double.Parse(input[0]), double.Parse(input[1])), double.Parse(input[2]));
+                if(result[0].ToLower() == "nt")
+                {
+                    Assert.AreEqual(0,ActualResult.Count);
+                }
+                else if(result[0].ToLower()=="everything")
+                {
+                    Assert.AreEqual(testCollection.Count,ActualResult.Count);
+                }
+                else
+                {
+                    Assert.AreEqual(result.Length, ActualResult.Count);
+                    foreach(var r in ActualResult)
+                    {
+                        Assert.IsTrue(result.Contains(r.LocationID));
+                    }
+                }
+            }
+        }
+        [TestMethod]
+        public void ClosestLocations()
+        {
+            string[] tests = Helpers.OpenFile("TestData/ClosestLocationsTestData.txt");
+            LocationTriggerCollection<BasicLocationTrigger> testCollection = new LocationTriggerCollection<BasicLocationTrigger>();
+            testCollection.AddRange(testData.TestData);
+            foreach (string s in tests)
+            {
+                string[] test = s.Split('\t');
+                string[] input = test[0].Split(',');
+                string[] result = test[1].Split(',');
+                var ActualResult = testCollection.ClosestLocations(new MapCoordinate(double.Parse(input[0]), double.Parse(input[1])), int.Parse(input[2]));
+                if (result[0].ToLower() == "everything")
+                {
+                    Assert.AreEqual(testCollection.Count, ActualResult.Count);
+                }
+                else
+                {
+                    Assert.AreEqual(result.Length, ActualResult.Count);
+                    foreach (var r in ActualResult)
+                    {
+                        Assert.IsTrue(result.Contains(r.LocationID));
+                    }
+                }
+            }
+        }
+        [TestMethod]
+        public void SortedLocations()
+        {
+            string[] tests = Helpers.OpenFile("TestData/ClosestLocationsTestData.txt");
+            LocationTriggerCollection<BasicLocationTrigger> testCollection = new LocationTriggerCollection<BasicLocationTrigger>();
+            testCollection.AddRange(testData.TestData);
+            int count = 0;
+            foreach (string s in tests)
+            {
+                string[] test = s.Split('\t');
+                string[] input = test[0].Split(',');
+                string[] expectedResults = test[1].Split(',');
+                var ActualResult = testCollection.ClosestLocations(new MapCoordinate(double.Parse(input[0]), double.Parse(input[1])), int.Parse(input[2]));
+                if (expectedResults[0].ToLower() == "everything")
+                {
+                    Assert.AreEqual(testCollection.Count, ActualResult.Count);
+                }
+                else
+                {
+                    Assert.AreEqual(expectedResults.Length, ActualResult.Count);
+                    for (int i= 0;i<expectedResults.Length;i++)
+                    {
+                        Assert.AreEqual(expectedResults[i], ActualResult[i].LocationID);
+                    }
+                }
+                count++;
+            }
+        }
+        [TestInitialize]
+        public void IntializeTests()
+        {
+            testData = new Data.TestLocationTriggerData();
         }
 
     }
